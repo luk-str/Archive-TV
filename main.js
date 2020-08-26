ce49b1b4149255e8a932760f717befa5eac17585
luk-str <lstrociak@gmail.com>
2020-08-26 20:57:32 +0200

    Add Reload Button

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function getRandomFilm(films) {
 
 function generateFetchUrl() {
     const randomPageNumber = Math.floor(Math.random() * 1000);
-    return `https://archive.org/advancedsearch.php?q=collection%3A%28moviesandfilms%29+AND+mediatype%3A%28movies%29&fl%5B%5D=identifier&fl%5B%5D=title&fl%5B%5D=date&sort%5B%5D=__random+desc&sort%5B%5D=&sort%5B%5D=&rows=10&page=${randomPageNumber}&output=json`
+    return `https://archive.org/advancedsearch.php?q=-title%3A%28filmcollectief%29+AND+collection%3A%28moviesandfilms%29+AND+mediatype%3A%28movies%29&fl%5B%5D=identifier&fl%5B%5D=title&fl%5B%5D=date&sort%5B%5D=__random+desc&sort%5B%5D=&sort%5B%5D=&rows=10&page=${randomPageNumber}&output=json`
 }
 
 fetch(
@@ -23,7 +23,7 @@ fetch(
     document.getElementById("title").innerHTML = `title: <strong>${randomFilm.title}</strong>`;
     document.getElementById("year").innerHTML = `year: <strong>${filmDate}</strong>`;
 
-    const filmUrl = `https://archive.org/embed/${randomFilm.identifier}&autoplay=0`;
+    const filmUrl = `https://archive.org/embed/${randomFilm.identifier}&autoplay=1`;
 
     document.getElementById("loading").remove();
     document.getElementById("player").setAttribute("src", filmUrl);
