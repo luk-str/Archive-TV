acd6162d6a940fc9941caa1023db6130e85116f1
luk-str <lstrociak@gmail.com>
2020-08-29 15:25:01 +0200

    Replace IA Player With Plyr

--- main.js
+++ main.js
@@ -1,30 +1,95 @@
+function generateSearchURL() {
+  const randomPageNumber = Math.floor(Math.random() * 1000);
+  return `https://archive.org/advancedsearch.php?q=-title:(filmcollectief,+stock+footage)+AND+collection:(moviesandfilms)+AND+mediatype:(movies)&fl[]=identifier&fl[]=title&fl[]date&sort[]=__random+desc&sort[]=&sort[]=&rows=10&page=${randomPageNumber}&output=json`;
+}
+
 function getRandomFilm(films) {
   const randomId = Math.floor(Math.random() * films.length);
   return films[randomId];
 }
 
-function generateFetchUrl() {
-    const randomPageNumber = Math.floor(Math.random() * 1000);
-    return `https://archive.org/advancedsearch.php?q=-title%3A%28filmcollectief%29+AND+collection%3A%28moviesandfilms%29+AND+mediatype%3A%28movies%29&fl%5B%5D=identifier&fl%5B%5D=title&fl%5B%5D=date&sort%5B%5D=__random+desc&sort%5B%5D=&sort%5B%5D=&rows=10&page=${randomPageNumber}&output=json`
+const playerOptions = {
+  autoplay: false,
+  controls: [
+    "play-large",
+    "play",
+    "progress",
+    "current-time",
+    "volume",
+    "airplay",
+    "fullscreen",
+  ],
+};
+
+function runPlayer(randomFilm) {
+  const identifier = randomFilm.identifier;
+  const filesUrl = `https://archive.org/metadata/${identifier}/files`;
+  fetch(filesUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const result = data.result;
+      let fileh264 = "";
+      let fileMPEG4 = "";
+      let fileQuickTime = "";
+      let fileOgg = "";
+
+      for (let i = 0; i < result.length; i++) {
+        let fileName = result[i].name;
+        switch (result[i].format) {
+          case "h.264":
+            fileh264 = fileName;
+            break;
+          case "MPEG4":
+            fileMPEG4 = fileName;
+            break;
+          case "QuickTime":
+            fileQuickTime = fileName;
+            break;
+          case "Ogg Video":
+            fileOgg = fileName;
+        }
+      }
+
+      const playerSource = {
+        type: "video",
+        sources: [
+          {
+            src: `https://archive.org/download/${identifier}/${
+              fileh264 || fileMPEG4 || fileQuickTime
+            }`,
+            type: "video/mp4",
+          },
+          {
+            src: `https://archive.org/download/${identifier}/${fileOgg}`,
+            type: "video/ogg",
+          },
+        ],
+      };
+      const player = new Plyr("#player", playerOptions);
+      player.source = playerSource;
+    });
 }
 
-fetch(
-    generateFetchUrl()
-)
-  .then((response) => response.json())
-  .then((data) => {
-    const films = data.response.docs;
+function startFilm() {
+  fetch(generateSearchURL())
+    .then((response) => response.json())
+    .then((data) => {
+      const films = data.response.docs;
+      const randomFilm = getRandomFilm(films);
+      const filmDate = new Date(randomFilm.date).getFullYear() || "undefined";
+      const title = randomFilm.title;
 
-    const randomFilm = getRandomFilm(films);
-    console.log(randomFilm.title);
-    const filmDate = new Date(randomFilm.date).getFullYear() || "unknown";
-    console.log(filmDate);
+      document.getElementById(
+        "title"
+      ).innerHTML = `title: <strong>${title}</strong>`;
+      document.getElementById(
+        "year"
+      ).innerHTML = `year: <strong>${filmDate}</strong>`;
 
-    document.getElementById("title").innerHTML = `title: <strong>${randomFilm.title}</strong>`;
-    document.getElementById("year").innerHTML = `year: <strong>${filmDate}</strong>`;
+      document.getElementById("loading").remove();
 
-    const filmUrl = `https://archive.org/embed/${randomFilm.identifier}&autoplay=1`;
+      runPlayer(randomFilm);
+    });
+}
 
-    document.getElementById("loading").remove();
-    document.getElementById("player").setAttribute("src", filmUrl);
-  });
+startFilm();
