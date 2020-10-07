e1f193d879f810d2ee90660bb06f00a12f46ae44
luk-str <lstrociak@gmail.com>
2020-10-07 19:15:25 +0200

    Remove Collection Selector

--- script/main.js
+++ script/main.js
@@ -2,7 +2,6 @@ const reloadButton = document.getElementById("reloadButton");
 
 const titleText = document.getElementById("titleText");
 const sourceText = document.getElementById("sourceText");
-const collectionText = document.getElementById("collectionText");
 const yearMetadata = document.getElementById("yearMetadata");
 const descriptionMetadata = document.getElementById("descriptionMetadata");
 
@@ -16,7 +15,7 @@ const randomPageNumber = () => {
   return Math.floor(Math.random() * searchPagesRange);
 };
 
-const getSearchUrl = (collectionName) => {
+const getSearchUrl = () => {
   const searchUrl = `https://archive.org/advancedsearch.php?q=-title:(filmcollectief,+stock+footage)+AND+collection:(${collectionName})+AND+mediatype:(movies)&sort[]=__random+desc&sort[]=&sort[]=&rows=${resultsAmount}&page=${randomPageNumber()}&output=json`;
 
   console.log(searchUrl);
@@ -36,7 +35,7 @@ const playerOptions = {
 
 function onPlayerLoaded() {
   document.getElementById("loading").remove();
-  loadVideo(collectionName);
+  loadVideo();
 }
 
 // Define Player Window
@@ -48,24 +47,23 @@ const player = videojs(
 
 // Trigger Video Reload on Error
 player.on("error", () => {
-  loadVideo(collectionName);
+  loadVideo();
 });
 
 // Trigger Video Reload on Video End
 player.on("ended", () => {
-  loadVideo(collectionName);
+  loadVideo();
 });
 
 // Handle Button Clicks
 reloadButton.onclick = () => {
-  loadVideo(collectionName);
+  loadVideo();
 };
 
 // Load Video
-async function loadVideo(collectionName) {
-  console.log(collectionName);
+async function loadVideo() {
   player.pause();
-  const response = await fetch(getSearchUrl(collectionName));
+  const response = await fetch(getSearchUrl());
   const data = await response.json();
   const filmMetadata = data.response.docs[0];
   console.log(filmMetadata);
@@ -84,12 +82,6 @@ function fillMetaData(film) {
   const description = film.description;
   const id = film.identifier;
   const sourceUrl = `https://archive.org/details/${id}`;
-
-  switch (collectionName) {
-    case "moviesandfilms":
-      collectionText.textContent = "Movies and Films";
-      break;
-  }
 
   titleText.textContent = title;
   sourceText.textContent = sourceUrl;
